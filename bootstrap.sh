6f4eb446861c51d81d34e2f565d37c191629a5cc
Noah Hummel <hello@noahhummel.consulting>
2020-03-20 21:19:28 +0100

    INIT

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -L https://bootstrap.saltstack.com -o bootstrap-salt.sh
+sha256sum bootstrap-salt.sh
+
+echo "NOW LISTEN UP YER LIL MAGGOT: yer gonna go to https://github.com/saltstack/salt-bootstrap and check the bloody checksum before you type yes, aye?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) sh bootstrap-salt.sh -x python3; break;;
+        No ) exit;;
+    esac
+done
